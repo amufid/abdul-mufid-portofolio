157e00f14c01d7359dda1dbceb01e03596a317fb
Abdul Mufid <91891129+amufid@users.noreply.github.com>
2024-07-09 13:51:07 +0700

    Animation (#2)
    * update image src
    
    * add animations

---
+++ src/animated/AnimatedComponent.jsx
@@ -0,0 +1,28 @@
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+
+const AnimatedSection = ({ children }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const variants = {
+    hidden: { opacity: 0, y: 200 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={inView ? 'visible' : 'hidden'}
+      variants={variants}
+      transition={{ duration: 1.5 }}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default AnimatedSection;
---
+++ src/lib/SwitchTheme.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { IoMoon } from "react-icons/io5";
+import { IoSunny } from "react-icons/io5";
+
+function SwitchTheme() {
+   const [dark, setDark] = useState(false);
+
+   useEffect(() => {
+      const savedTheme = localStorage.getItem("dark-mode");
+      if (savedTheme === "true") {
+         setDark(true);
+         document.body.classList.add("dark");
+      } else {
+         setDark(false);
+         document.body.classList.remove("dark");
+      }
+   }, []);
+
+   const darkModeHandler = () => {
+      setDark(!dark);
+      if (!dark) {
+         document.body.classList.add("dark");
+         localStorage.setItem("dark-mode", "true");
+      } else {
+         document.body.classList.remove("dark");
+         localStorage.setItem("dark-mode", "false");
+      }
+   }
+
+   return (
+      <div className="ml-2">
+         <button onClick={() => darkModeHandler()}>
+            {
+               dark && <IoSunny className="h-6 w-6 text-yellow-100" />
+            }
+            {
+               !dark && <IoMoon className="h-6 w-6 text-white" />
+            }
+         </button>
+      </div>
+   );
+}
+
+export default SwitchTheme;
