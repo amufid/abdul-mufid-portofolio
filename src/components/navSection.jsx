606159482c431e97b814d590a9685669a64f46d6
Abdul Mufid <91891129+amufid@users.noreply.github.com>
2024-10-11 15:15:09 +0700

    Navbar v1 (#16)
    * optimization page view
    
    * update nav and feat: add new project
    
    * update font and update section projects
    
    * feat: seo optimization and responsive page
    
    * update link icon at head
    
    * chore: update react icons at footer
    
    * update link linkedin
    
    * update navbar and background color
    
    * delete code in index.css
    
    * update style border
    
    * update navbar style

--- src/components/navSection.jsx
+++ src/components/navSection.jsx
@@ -71,7 +71,7 @@ export default function NavSection(props) {
             <SwitchTheme />
           </div>
           {/* mobile mode  */}
-          <div className="flex sm:hidden ml-32 mr-5 mt-[5px] mb-[-2px]">
+          <div className="flex sm:hidden ml-32 mr-5">
             <SwitchTheme />
           </div>
           <div className="flex sm:hidden dark:text-slate-200">
