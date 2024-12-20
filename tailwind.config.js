7de7fccbefe985c2eb6a117b8299753d18e5bf97
amufid <91891129+amufid@users.noreply.github.com>
2024-12-20 10:51:35 +0700

    chore: update projects and cover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'bg-image': 'url("/public/blue2.jpg")',
-      }
+        "bg-image": 'url("/images/bg-modern.jpg")',
+      },
     },
   },
   darkMode: "class",
   plugins: [],
-}
-
+};
